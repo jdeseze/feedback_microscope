633b2410e508548ee09931314686902674298c5e
jdeseze <68115566+jdeseze@users.noreply.github.com>
2020-07-24 17:20:13 +0200

    Add files via upload

--- feedback_functions.py
+++ feedback_functions.py
@@ -6,24 +6,84 @@ Created on Wed Jul 15 15:58:48 2020
 """
 import streamlit as st
 import time
-import handles as h
+import classes as cl
+from pycromanager import Bridge
+from PIL import Image
+import numpy as np
+import types
+from datetime import datetime
+import updated_variables as uv
+import pandas as pd
 
-def print_time(a):
-    st.write(a)
-    
-def hello_world():
-    st.write("Hello World")
 
 @st.cache    
-def startFcn(handles):
+def start_fcn(handles):
     #timer=time.time
     #init_time=int(timer)
-    st.write('startFcn')
-    return time.time()-handles.init_time
-
-@st.cache
-def initFcn():   
-    handles=h.handles
-    timer=time.time()
-    handles.init_time=int(timer)
+    handles.testval=time.perf_counter()-handles.init_time
     return handles
+
+@st.cache(allow_output_mutation=True)
+def init_fcn(rand):
+    #handles=h.handles()
+    path=uv.__file__
+    with open(path,"w") as fp:
+        fp.writelines(['a=3 \n','b=12 \n'])
+    handles=types.SimpleNamespace()
+    handles.init_time=0
+    handles.test=1
+    handles.disp_image=Image.open('test.png')
+    handles.current_time_step=0
+    handles.testval=0
+    core=Bridge().get_core()
+    handles.core=core
+    handles.testval=0
+    handles.num_pos=2
+    handles.all_pos='./all_pos.pkl'
+    all_pos=pd.DataFrame(np.zeros([1,3]),columns=['x', 'y', 'z'])
+    all_pos.to_pickle('./all_pos.pkl')
+    handles.current_step=0
+    handles.num_steps=0
+    handles.num_WL=1
+    handles.current_WL=1
+    handles.session_id=np.random.rand()
+    return handles
+
+#@st.cache
+def start_acq(handles):
+    handles.acq=cl.PT(1,printer)
+    handles.acq.start()
+    #return acq
+
+def stop_acq(handles):
+    handles.acq.cancel()
+
+def printer():
+    tempo = datetime.today()
+    h,m,s = tempo.hour, tempo.minute, tempo.second
+    time=(f"{h}:{m}:{s}")
+    path=uv.__file__
+    with open(path,"w") as fp:
+        fp.writelines(['a=5 \n','b="'+time+'" \n'])
+    #return st.write((f"{h}:{m}:{s}"))
+
+#@st.cache
+def acq_fcn(handles):
+    handles.core.snap_image()
+    tagged_img=handles.core.get_tagged_image()
+    pixvals=np.reshape(tagged_img.pix,newshape=[tagged_img.tags['Height'], tagged_img.tags['Width']])
+    pixvals = ((pixvals - pixvals.min()) / (pixvals.max()-pixvals.min())) 
+    handles.disp_image=pixvals
+    #time.sleep(1)
+    #return handles
+    
+@st.cache
+def get_table(string,num_pos,rand):
+    return pd.read_pickle(string)
+
+
+def add_pos(core,string):
+    current_pos=pd.DataFrame(np.array([[core.get_x_position(),core.get_y_position(),core.get_position()]]),columns=['x', 'y', 'z'])
+    all_pos=pd.read_pickle(string)
+    all_pos=all_pos.append(current_pos,ignore_index=True)
+    all_pos.to_pickle(string)
--- test_streamlit.py
+++ test_streamlit.py
@@ -7,41 +7,71 @@ Ceci est un script temporaire.
 
 import streamlit as st
 import feedback_functions as ff
+import updated_variables as uv
+import numpy as np
+import time
+import pycromanager as pm
+from PIL import Image
+import copy
+import classes as cl
+import pandas as pd
 
-handles=ff.initFcn()
+handles=ff.init_fcn(0)
+
+st.title("Test acquisition function")
+st.write("Current time step : ",handles.current_time_step)
+
 st.write(handles.init_time)
-
-time_step=st.number_input("Time step (in s)",0,None,0)
-nb_steps=st.number_input("Number of steps",0,None,0)
-
 
 st.slider('test',0,50)
 
 #a=st.sidebar.selectbox('test1',('proposition 1','der'))
 
-timebutton=st.button("hola")
-
-if timebutton:
-    st.write("blague")
-
+if st.button('initialize again'):
+    handles.session_id=np.random.rand()
+    ff.init_fcn(handles.session_id)
 
 check=st.checkbox('this is a test')
-
 if check:
-    "holaaaa"
+    path=uv.__file__
+    with open(path,"w") as fp:
+        fp.writelines(['a=5 \n','b=7 \n'])
 
-if st.button("ohlala"):
-    #ff.print_time(init_time)
-    ff.hello_world()
-    st.write(handles.init_time)
 
-start_button=st.button('Start')
-if start_button:
-    testval=ff.startFcn(handles)
-    st.write(testval)
+st.write(uv.a)
+
+if st.button('Start acquisition'):
+    ff.start_acq(handles)
     
+st.sidebar.header(uv.b)
+        
+
+# =============================================================================
+# t=h.PT(1,ff.printer(handles))
+# t.start()
+# 
+# =============================================================================
+if st.button('Stop acquisition'):
+    ff.stop_acq(handles)
 
 
+nbsteps=st.sidebar.number_input('Number of steps',1)
+timestep=st.sidebar.number_input('Time step (in s)',1)    
+
+if st.button('Acquire'):
+    ff.acq_fcn(handles)
+    #handles.disp_image=Image.open('test.png')
+    
+st.image(handles.disp_image,caption='blablabla',use_column_width=True,format='PNG')
+
+if st.button('Add position'):
+    ff.add_pos(handles.core,handles.all_pos)
+    handles.num_pos=handles.num_pos+1
+    
+dataframe = ff.get_table(handles.all_pos,handles.num_pos,handles.session_id)
+st.write(dataframe)
+selected_indices = st.selectbox('Select rows:', dataframe.index)
+selected_rows = dataframe.loc[selected_indices]
 # =============================================================================
 # @st.cache  # 👈 This function will be cached
 # def my_slow_function(arg1, arg2):
@@ -50,3 +80,27 @@ if start_button:
 # 
 # =============================================================================
 
+# =============================================================================
+# progress_bar = st.progress(0)
+# status_text = st.empty()
+# chart = st.line_chart(np.random.randn(10, 2))
+# 
+# for i in range(100):
+#     # Update progress bar.
+#     progress_bar.progress(i + 1)
+# 
+#     new_rows = np.random.randn(10, 2)
+# 
+#     # Update status text.
+#     status_text.text(
+#         'The latest random number is: %s' % new_rows[-1, 1])
+# 
+#     # Append data to the chart.
+#     chart.add_rows(new_rows)
+# 
+#     # Pretend we're doing some computation that takes time.
+#     time.sleep(0.1)
+# 
+# status_text.text('Done!')
+# st.balloons()
+# =============================================================================
